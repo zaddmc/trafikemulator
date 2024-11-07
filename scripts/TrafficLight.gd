7687534a98c7a9da5298f67771b6d7d707768307
Alehandre420 <alexandernikolajlarsen@gmail.com>
2024-11-07 14:20:54 +0100

    This shit almost works

--- scripts/TrafficLight.gd
+++ scripts/TrafficLight.gd
@@ -8,6 +8,7 @@ var iswaiting = false
 var Colorred = Color.hex(0xfa0a0aff)
 var Colorgreen = Color.hex(0x1adb14ff)
 var new_material = StandardMaterial3D.new()
+var green_material = StandardMaterial3D.new()
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,7 +17,7 @@ func _ready() -> void:
 	roads.remove_at(0)
 	for n in roads.size():
 		roadselect.append(false)
-	Switch_Ligts_Color()
+	
 
 func wait(seconds: float) -> void:
 	await get_tree().create_timer(seconds).timeout
@@ -26,7 +27,7 @@ func update_trafficlight():
 	if iswaiting == false:
 		iswaiting = true
 		for i in range(roadselect.size()): roadselect[i] = false 
-		Switch_Ligts_Color()
+		Switch_Ligts_Color(lightselect)
 		print(roadselect)
 		await wait(2)
 		roadselect[lightselect-1] = true
@@ -35,19 +36,20 @@ func update_trafficlight():
 		else:
 			lightselect = 0
 		iswaiting = false
-		Switch_Ligts_Color()
+		Switch_Ligts_Color(lightselect+1)
 		print(roadselect)
 		return
 	return
-func Switch_Ligts_Color():
-	if true in roadselect:
-		new_material.albedo_color = Colorgreen
-		get_child(0).get_child(1).material_override = new_material
-		print("balls")
-	else:
-		new_material.albedo_color = Colorred
-		get_child(0).get_child(1).material_override = new_material
-		print("penis")
-	
 
+func Switch_Ligts_Color(light: int):
+	print(light)
+	if roadselect.has(true):
+		green_material.albedo_color = Colorgreen
+		get_child(0).get_child(light+1).material_override = green_material
+		print("balls")
+	elif roadselect.has(true) == false:
+		new_material.albedo_color = Colorred
+		for n in range(len(roadselect)):
+			get_child(0).get_child(n+1).material_override = new_material
+			print("penis")
 		
